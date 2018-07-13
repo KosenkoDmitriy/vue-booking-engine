c75b660ee9ac4994f74887b0a8e5179dacc63c3e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-13 20:54:51 +0300

    Simple UI markup for a list items

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -20,7 +20,7 @@
         <v-card
           class="mb-5"
           color="grey lighten-1"
-          height="200px">
+          height="auto">
           <v-layout wrap>
             <v-flex xs5 sm4 d-flex>
               <v-select
@@ -37,6 +37,49 @@
                 label="Select Building Type"></v-select>
             </v-flex>
           </v-layout>
+
+
+       <v-list three-line>
+         <template v-for="(item, index) in items">
+           <v-subheader
+             v-if="item.header"
+             :key="item.header"
+           >
+             {{ item.header }}
+           </v-subheader>
+
+           <v-divider
+             v-else-if="item.divider"
+             :inset="item.inset"
+             :key="index"
+           ></v-divider>
+
+           <v-list-tile
+             v-else
+             :key="item.title"
+             avatar
+             @click=""
+           >
+             <v-list-tile-avatar>
+               <img :src="item.avatar" ><!--  style="height:100px;width:100px;"> -->
+             </v-list-tile-avatar>
+
+             <v-list-tile-content>
+               <v-list-tile-title v-html="item.title"></v-list-tile-title>
+               <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+               <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+             </v-list-tile-content>
+             <v-list-tile-action>
+               <v-btn :color="item.active ? 'teal' : 'blue'">Add Extras</v-btn>
+               <v-list-tile-sub-title>Room Hire Cost-Half Day: $300</v-list-tile-sub-title>
+               <v-list-tile-sub-title>Extras: - </v-list-tile-sub-title>
+               <v-list-tile-title>Total: $300</v-list-tile-title>
+               <v-btn :color="item.active ? 'teal' : 'green'">Book And Confirm</v-btn>
+               <!-- <v-btn :color="item.active ? 'teal' : 'blue'">Hold</v-btn> -->
+            </v-list-tile-action>
+           </v-list-tile>
+         </template>
+       </v-list>
         </v-card>
 
         <v-btn
@@ -93,8 +136,43 @@ export default {
       return {
         eventList: ['Meeting', 'Wedding'],
         buildList: ['Theater',],
-        e1: 0
+        e1: 0,
+        items: [
+          // { header: 'Today' },
+          {
+            avatar: './avatar.png',
+            title: 'Office',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: './avatar.png',
+            title: 'Flat <span class="grey--text text--lighten-1">50</span>',
+            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+          },
+          { divider: true, inset: true },
+          {
+            avatar: './avatar.png',
+            title: 'Restaurant',
+            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+          }
+        ]
       }
     }
 }
 </script>
+<style>
+.v-list__tile {
+  height: 180px !important;
+}
+.v-avatar {
+  width: 160px !important;
+  height: 160px !important;
+}
+.v-avatar img {
+  border-radius: 5%;
+}
+.v-list__tile__content {
+    padding-left: 85px;
+}
+</style>
