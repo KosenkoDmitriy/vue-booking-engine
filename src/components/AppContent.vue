7bbdb2f53e6ae06de8d08d1bb398ca01f5f66da5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-15 16:02:40 +0300

    Event Details Screen (step 1)

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,21 +1,101 @@
 <template>
-<v-content>
-<v-container fluid>
+<v-content><v-container fluid>
   <v-stepper v-model="e1">
     <v-stepper-header>
       <v-stepper-step :complete="e1 > 1" step="1">Event Details</v-stepper-step>
-
       <v-divider></v-divider>
-
       <v-stepper-step :complete="e1 > 2" step="2">Select Package</v-stepper-step>
-
       <v-divider></v-divider>
-
       <v-stepper-step step="3">Book and Pay </v-stepper-step>
     </v-stepper-header>
 
     <v-stepper-items>
       <v-stepper-content step="1">
+        <v-card
+          class="mb-5"
+          color="grey lighten-1"
+          height="auto">
+          <v-layout wrap>
+            <v-flex xs5 sm4 d-flex>
+              <v-select
+                :items="eventList"
+                label="Select Event"></v-select>
+            </v-flex>
+            <v-flex xs2 sm4 d-flex>
+              <v-text-field
+                label="Number of Quests"
+                value="10"></v-text-field>
+            </v-flex>
+            <v-flex xs5 sm4 d-flex>
+              <v-select
+                :items="buildList"
+                label="Select Building Type"
+                ></v-select>
+            </v-flex>
+            <v-flex xs12 sm6 md4>
+              <v-menu
+                ref="menu2"
+                :close-on-content-click="false"
+                v-model="menu2"
+                :nudge-right="40"
+                :return-value.sync="date"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                min-width="290px"
+              >
+                <v-text-field
+                  slot="activator"
+                  v-model="date"
+                  label="Select Date"
+                  prepend-icon="event"
+                  readonly
+                ></v-text-field>
+                <v-date-picker v-model="date" @input="$refs.menu2.save(date)"></v-date-picker>
+              </v-menu>
+            </v-flex>
+            <v-flex xs11 sm5>
+      <v-menu
+        ref="menu"
+        :close-on-content-click="false"
+        v-model="menu2"
+        :nudge-right="40"
+        :return-value.sync="time"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        max-width="290px"
+        min-width="290px"
+      >
+        <v-text-field
+          slot="activator"
+          v-model="time"
+          label="Picker in menu"
+          prepend-icon="access_time"
+          readonly
+        ></v-text-field>
+        <v-time-picker v-model="time" @change="$refs.menu.save(time)"></v-time-picker>
+      </v-menu>
+    </v-flex>
+    <v-spacer></v-spacer>
+
+          </v-layout>
+          <v-card-actions style="text-xs-right">
+            <v-btn
+              color="primary"
+
+              @click="e1 = 2">
+              Find Venue
+            </v-btn>
+            <!-- <v-btn flat>Cancel</v-btn> -->
+          </v-card-actions>
+        </v-card>
+
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
         <v-card
           class="mb-5"
           color="grey lighten-1"
@@ -37,66 +117,52 @@
             </v-flex>
           </v-layout>
 
-       <v-list three-line class="itemList">
-         <template v-for="(item, index) in items">
-           <extra-table v-if="item.id"
-            v-bind:isVisible=false v-bind:id="index" v-on:calculateTotal="calculateTotalSum" :key="item.id">
-           </extra-table>
-           <v-subheader
-             v-if="item.header"
-             :key="item.header"
-           >
-             {{ item.header }}
-           </v-subheader>
+          <v-list three-line class="itemList">
+            <template v-for="(item, index) in items">
+              <extra-table v-if="item.id"
+                v-bind:isVisible=false v-bind:id="index"
+                v-on:calculateTotal="calculateTotalSum"
+                :key="item.id">
+              </extra-table>
+               <v-subheader
+                 v-if="item.header"
+                 :key="item.header"
+               >
+                 {{ item.header }}
+               </v-subheader>
 
-           <v-divider
-             v-if="item.divider"
-             :inset="item.inset"
-             :key="'divider'+index"
-           ></v-divider>
+               <v-divider
+                 v-if="item.divider"
+                 :inset="item.inset"
+                 :key="'divider'+index"
+               ></v-divider>
 
-           <v-list-tile
-             v-else
-             :key="item.title"
-             avatar
-             @click=""
-           >
+               <v-list-tile
+                 v-else
+                 :key="item.title"
+                 avatar
+                 @click=""
+               >
 
-             <v-list-tile-avatar>
-               <img :src="item.avatar">
-             </v-list-tile-avatar>
+               <v-list-tile-avatar>
+                 <img :src="item.avatar">
+               </v-list-tile-avatar>
 
-             <v-list-tile-content>
-               <v-list-tile-title v-html="item.title"></v-list-tile-title>
-               <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-               <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-             </v-list-tile-content>
-             <v-list-tile-action>
-               <!-- <v-btn v-on:click="isExtraVisible = true" :color="item.active ? 'teal' : 'blue'">Hold</v-btn> -->
-               <v-list-tile-sub-title>Room Hire Cost/Day: $ {{ item.total }}</v-list-tile-sub-title>
-               <v-list-tile-sub-title>Extras: $ {{ item.totalExtra }} </v-list-tile-sub-title>
-               <v-list-tile-title>Total: $ {{ calculateTotalWithExtraPkg(item) }}</v-list-tile-title>
-               <v-btn :color="item.active ? 'teal' : 'green'">Book And Confirm</v-btn>
-            </v-list-tile-action>
-           </v-list-tile>
-         </template>
-       </v-list>
-        </v-card>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 2">
-          Continue
-        </v-btn>
-
-        <v-btn flat>Cancel</v-btn>
-      </v-stepper-content>
-
-      <v-stepper-content step="2">
-        <v-card
-          class="mb-5"
-          color="grey lighten-1"
-          height="200px">
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                  <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <!-- <v-btn v-on:click="isExtraVisible = true" :color="item.active ? 'teal' : 'blue'">Hold</v-btn> -->
+                  <v-list-tile-sub-title>Room Hire Cost/Day: $ {{ item.total }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>Extras: $ {{ item.totalExtra }} </v-list-tile-sub-title>
+                  <v-list-tile-title>Total: $ {{ calculateTotalWithExtraPkg(item) }}</v-list-tile-title>
+                  <v-btn v-on:click="e1 = 3" :color="item.active ? 'teal' : 'green'">Book And Confirm</v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </template>
+          </v-list>
         </v-card>
 
         <v-btn
@@ -105,7 +171,7 @@
           Continue
         </v-btn>
 
-        <v-btn flat>Cancel</v-btn>
+        <v-btn flat @click="e1 = 1">Back</v-btn>
       </v-stepper-content>
 
       <v-stepper-content step="3">
@@ -147,8 +213,19 @@ export default {
   },
   data () {
       return {
-        eventList: ['Meeting', 'Wedding'],
-        buildList: ['Theater', 'Restaurant', 'Office', 'Flat'],
+        //start date Picker
+        date: null,
+        menu: false,
+        modal: false,
+        // menu2: false,
+        //end date Picker
+        //start time picker
+        time: null,
+        menu2: false,
+        modal2: false,
+        //end time picker
+        eventList: ['Banquet', 'Cocktail', 'Conference', 'Meeting', 'Wedding'],
+        buildList: [ 'Cafe', 'Theater', 'Restaurant', 'Office', 'Flat'],
         e1: 0,
         items: [
           // { header: 'Today' },
@@ -156,8 +233,8 @@ export default {
             id: 1,
             avatar: './avatar.png',
             title: 'Office',
-            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-            total: 300,
+            subtitle: "<span class='text--primary'>255.5 sqm</span> <ul><li>Largest room</li><li>Blackout blinds</li><li>Doors leading onto a balcony</li></ul>",
+            total: 500,
             totalExtra: 0,
           },
           { divider: true, inset: true },
@@ -165,7 +242,7 @@ export default {
             id: 3,
             avatar: './avatar.png',
             title: 'Flat <span class="grey--text text--lighten-1">50</span>',
-            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+            subtitle: "<span class='text--primary'>155.5 sqm</span> &mdash; <ul><li>Largest room</li><li>Blackout blinds</li><li>Doors leading onto a balcony</li></ul>",
             total: 200,
             totalExtra: 0,
           },
@@ -174,8 +251,8 @@ export default {
             id: 5,
             avatar: './avatar.png',
             title: 'Restaurant',
-            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
-            total: 100,
+            subtitle: "<span class='text--primary'>555.5 sqm</span> &mdash; <ul><li>Largest room</li><li>Blackout blinds</li><li>Doors leading onto a balcony</li></ul>",
+            total: 900,
             totalExtra: 0,
           }
         ]
